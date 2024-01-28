53a03ee995034fbfce4df1577675fd7d6a6cf14b
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2024-01-28 22:10:47 +0530

    Starting javascript

---
+++ LEC54 - Introduction to JavaScript & Installing Node.js/script.js
@@ -0,0 +1,17 @@
+alert("Yo bro who r u?");
+
+console.log("u antavama ma uu")
+
+var a = prompt("Enter your number") //it used to take input 
+console.log(a)
+
+ var check = confirm("sab khatm kre ?") //confirm is like boolean in javascript
+if(check){
+    console.log("chaliye khatam kartte hai")
+}
+else{
+    console.log("rehne do ab")
+}
+
+document.title="besharam"
+document.body.style.backgroundColor = "pink" //it add inline css to element so it basically overwrite existing css

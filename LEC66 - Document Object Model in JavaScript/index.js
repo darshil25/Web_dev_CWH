ca0119d35ae740f6fea61e0ae9dcb6c8caf0d7a0
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2024-01-31 16:06:45 +0530

    Started DOM

---
+++ LEC66 - Document Object Model in JavaScript/index.js
@@ -0,0 +1,3 @@
+document.title = "Bharatbhai" //change doc titile
+document.body.style.backgroundColor = "pink" //add inline css
+console.log(document.body); //gives tree linke structure of html body

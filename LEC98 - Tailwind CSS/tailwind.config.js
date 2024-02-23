c19739ef35c2b1947ac3b0e4187d3add62acfa88
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2024-02-23 23:42:05 +0530

    lec98 - tailwind css

---
+++ LEC98 - Tailwind CSS/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"], //this says look every file that have .html or .css
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

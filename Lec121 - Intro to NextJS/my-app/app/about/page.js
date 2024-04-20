d4e62781437275f65940787ce222931ded4e3cad
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2024-04-20 16:55:14 +0530

    LEC 121 ADDED AFTER A DECADE

---
+++ Lec121 - Intro to NextJS/my-app/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae saepe consequuntur sunt neque corrupti quas!</h1>
+    </div>
+  )
+}
+
+export default page
